425886d523a8b99f902f72d480026f1c6b94448d
BYM-1020 <bym2013@outlook.com>
2023-02-06 12:43:48 +0100

    add Nic

--- nic.tf
+++ nic.tf
@@ -14,6 +14,6 @@ resource "azurerm_network_interface" "myTerraformnic" {
 }
 
   resource "azurerm_network_interface_security_group_association" "example" {
-  network_interface_id      = azurerm_network_interface.example.id
+  network_interface_id      = azurerm_network_interface.myTerraformnic.id
   network_security_group_id = azurerm_network_security_group.myterraformnsg.id
 }
