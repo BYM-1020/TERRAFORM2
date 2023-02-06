c526eb7b0431777b965af2c4d2c0ee1a5250c2d3
BYM-1020 <bym2013@outlook.com>
2023-02-06 12:41:19 +0100

    add Nic

--- nic.tf
+++ nic.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "myTerraformnic" {
   name                = var.nicname
   location            = var.location
   resource_group_name = azurerm_resource_group.myTerraformGroup.name
