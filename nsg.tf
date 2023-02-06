3fdce24148585ed5b89eb7b2b70fd54b04b584f0
BYM-1020 <bym2013@outlook.com>
2023-02-06 12:10:17 +0100

    add NSG

--- nsg.tf
+++ nsg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_network_security_group" "my terraformnsg" {
+resource "azurerm_network_security_group" "myterraformnsg" {
   name                = var.nsgname
   location            = var.location
   resource_group_name = azurerm_resource_group.myTerraformGroup.name
